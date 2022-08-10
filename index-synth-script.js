5bbec53ee1e0e4477a79bf85b4ea2f554a32c9af
James Koga <jyk54@cornell.edu>
2022-08-10 14:35:30 -0400

    added some fixes to button press

--- index-synth-script.js
+++ index-synth-script.js
@@ -3,11 +3,28 @@ const synth = new Tone.Synth().toDestination();
 
 const test = document.getElementById('test-button');
 
+const melody =
+  [{ note: "E5", duration: "8n", timing: 0 },
+  { note: "D#5", duration: "8n", timing: 0.25 },
+  { note: "E5", duration: "8n", timing: 0.5 },
+  { note: "D#5", duration: "8n", timing: 0.75 },
+  { note: "E5", duration: "8n", timing: 1 },
+  { note: "B4", duration: "8n", timing: 1.25 }]
+
 function playSound(button) {
   button.addEventListener("click", function (e) {
-    //play a middle 'C' for the duration of an 8th note
-    synth.triggerAttackRelease("C4", "8n");
-    console.log("button was clicked")
+    //play the melody:
+    melody.forEach(tune => {
+      const now = Tone.now();
+      synth.triggerAttackRelease(tune.note, tune.duration, now + tune.timing);
+    })
+
+
+    // melody.forEach(tune => {
+    //   const now = Tone.now()
+    //   synth.triggerAttackRelease(tune.note, tune.duration, now + tune.timing)
+    // })
+    console.log("button clicked")
   })
 }
 
